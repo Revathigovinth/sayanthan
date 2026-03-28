5d9f44740484f7d7f67c029fa05dcd9263be95be
Revathigovinth <your_email@gmail.com>
2026-03-28 21:42:24 +0530

    backend

--- backend/helloworld/src/main/java/com/example/helloworld/repository/helloworldRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.helloworld.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.helloworld.entity.helloworldEntity;
-
-public interface helloworldRepository extends JpaRepository<helloworldEntity,Long>{
-
-}
